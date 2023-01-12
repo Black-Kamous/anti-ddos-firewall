0baba8be1d61086146c627ea2f1f20ee7f4d2829
Black-Kamous <chengyu_kamous@outlook.com>
2023-01-12 13:20:16 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   core.h

--- core.h
+++ core.h
@@ -1,6 +1,26 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include <iostream>
+#include <cstdint>
+#include <fstream>
+
+#include "filter.h"
+#include "qn.h"
+
+// 捕包结果 simpack = <ip ttl qname ...>
+// 训练集 -> FQ(及时) UR, HC(提前定期)
+// 构造时读取训练完成结果？
+// 训练程序独立/引入
+//      主程序使用定时器
+// 
+// 全局变量 vec管理filter序列
+// 全局变量 运行simpack列表
+//
+// 函数 装载运行simpack
+// 函数 管理filter序列（有序）
+// 函数 测试filter序列（全部simpack）
+// 函数 测试filter序列（单个simpack）
 
 
 #endif
