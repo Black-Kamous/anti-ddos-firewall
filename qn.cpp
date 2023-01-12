3f9d4944c670fbcf64e7dcb019cab7447b77667d
Black-Kamous <chengyu_kamous@outlook.com>
2023-01-12 02:04:44 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   qn.cpp
    	modified:   qn.h
    
    Untracked files:
    	test_lpm
    	test_lpm.cpp

--- qn.cpp
+++ qn.cpp
@@ -1,5 +1,33 @@
 #include "qn.h"
+#include "plain_lpm.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <algorithm>
+
+
 
 QnFilter::QnFilter(std::string filename){
-    
+    lpm = new PlainLPM<struct qname, uint32_t>();
+    std::ifstream in(filename, std::ios::in);
+    std::string qname;
+    while(in.good()){
+        in >> qname;
+        std::reverse(qname.begin(), qname.end());
+        struct qname qn = {.n = {0}};
+        strncpy(qn.n, qname.c_str(), QNAME_MAX);
+        lpm->update(qn, qname.size(), 1);
+    }
+}
+
+int QnFilter::filter(std::string ip, int ipVer, std::string queryName, time_t time){
+    std::reverse(queryName.begin(), queryName.end());
+    struct qname qn = {.n = {0}};
+    strncpy(qn.n, queryName.c_str(), QNAME_MAX);
+    uint32_t* vptr = lpm->lookup(qn, queryName.size());
+    if(vptr == NULL){
+        nPassed++;
+    }else{
+        nDropped++;
+    }
 }
--- qn.h
+++ qn.h
@@ -1,13 +1,23 @@
-#include <types.h>
+#ifndef QN_H
+#define QN_H
+
+#include <cstdint>
 #include "filter.h"
 #include "lpm.h"
 
 #define QNAME_MAX 256
 
+struct qname{
+    char n[QNAME_MAX];
+};
+
 class QnFilter : public Filter
 {
-    LPM<char[QNAME_MAX], __u32> *lpm;
+    LPM<struct qname, uint32_t> *lpm;
 public:
     QnFilter(std::string filename);
+    int filter(std::string ip, int ipVer, std::string queryName, time_t time);
     
 };
+
+#endif
